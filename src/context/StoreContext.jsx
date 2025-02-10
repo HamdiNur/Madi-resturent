a48d72fdf35717c5f916554164ef5ceddf707b15
HamdiNur <hamdoonur26@gmail.com>
2025-02-10 10:52:07 +0300

    added place order page

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -19,18 +19,33 @@ const StoreContextProvider =(props)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    
-    useEffect(()=>{
-        console.log(cartItems)
+const getTotalCartAmount=()=>{
+    let totalAmount=0;
+    for(const item in cartItems)
+    {
+        if(cartItems[item]>0){
+        let itemInfo=food_list.find((product)=>product._id===item)
+        totalAmount+=itemInfo.price*cartItems[item];
 
-    },[cartItems])
+        }
+    }
+    return totalAmount;
+
+}
+
+    
+    // useEffect(()=>{
+    //     console.log(cartItems)
+
+    // },[cartItems])
     
     const contextValue={
       food_list,
       cartItems,
       setCartItems,
       addToCart,
-      removeFromCart
+      removeFromCart,
+      getTotalCartAmount
     }
 
     return (
